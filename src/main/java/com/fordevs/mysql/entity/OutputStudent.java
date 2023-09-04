a7f0549d5a16a39c94e7c96d3513ab5c0ddc75b1
Jocsan Mairena <jocsan.mairena@gmail.com>
2023-09-03 23:22:32 -0400

    Using lombok @Data and project rename to spring-batch-examples

--- src/main/java/com/fordevs/mysql/entity/OutputStudent.java
+++ src/main/java/com/fordevs/mysql/entity/OutputStudent.java
@@ -1,5 +1,7 @@
 package com.fordevs.mysql.entity;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -7,6 +9,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "student")
+@Data
 public class OutputStudent {
 
 	@Id
@@ -25,53 +28,5 @@ public class OutputStudent {
 
 	@Column(name = "is_active")
 	private Boolean isActive;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Long getDeptId() {
-		return deptId;
-	}
-
-	public void setDeptId(Long deptId) {
-		this.deptId = deptId;
-	}
-
-	public Boolean getIsActive() {
-		return isActive;
-	}
-
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
-	}
 
 }
